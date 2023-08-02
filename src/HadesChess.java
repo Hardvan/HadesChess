dc4b358f59f377ea3ecbeb0a537a34309322a1d6
KaranS09 <karan.sathish980@gmail.com>
2023-08-03 01:31:07 +0530

    Added the functionality for Bishops

--- src/HadesChess.java
+++ src/HadesChess.java
@@ -69,7 +69,40 @@ public class HadesChess {
         return list;
     }
     public static String posibleB(int i) {
-        String list="";
+        String list="", oldPiece;
+        int r=i/8, c=i%8;
+        int temp = 1;
+        for(int j=-1; j<=1; j+=2){
+            for(int k=-1; k<=1; k+=2){
+                if(j!=0 && k!=0) {
+                    try {
+                        while (" ".equals(chessBoard[r + temp * j][c + temp * k])) {
+                            oldPiece = chessBoard[r + temp * j][c + temp * k];
+                            chessBoard[r][c] = " ";
+                            chessBoard[r + temp * j][c + temp * k] = "B";
+                            if (kingSafe()) {
+                                list = list + r + c + (r + temp * j) + (c + temp * k) + oldPiece;
+                            }
+                            chessBoard[r][c] = "B";
+                            chessBoard[r + temp * j][c + temp * k] = oldPiece;
+                            temp++;
+                        }
+                        if ((Character.isLowerCase(chessBoard[r + temp * j][c + temp * k].charAt(0)))) {
+                            oldPiece = chessBoard[r + temp * j][c + temp * k];
+                            chessBoard[r][c] = " ";
+                            chessBoard[r + temp * j][c + temp * k] = "B";
+                            if (kingSafe()) {
+                                list = list + r + c + (r + temp * j) + (c + temp * k) + oldPiece;
+                            }
+                            chessBoard[r][c] = "B";
+                            chessBoard[r + temp * j][c + temp * k] = oldPiece;
+                        }
+                    } catch (Exception e) {
+                    }
+                    temp = 1;
+                }
+            }
+        }
         return list;
     }
     public static String posibleQ(int i) {
@@ -78,30 +111,33 @@ public class HadesChess {
         int temp = 1;
         for(int j=-1; j<=1; j++){
             for(int k=-1; k<=1; k++){
-                try {
-                    while(" ".equals(chessBoard[r+temp*j][c+temp*k])) {
-                        oldPiece = chessBoard[r+temp*j][c+temp*k];
-                        chessBoard[r][c] = " ";
-                        chessBoard[r+temp*j][c+temp*k] = "Q";
-                        if (kingSafe()) {
-                            list=list+r+c+(r+temp*j)+(c+temp*k)+oldPiece;
+                if(j!=0 || k!=0) {
+                    try {
+                        while (" ".equals(chessBoard[r + temp * j][c + temp * k])) {
+                            oldPiece = chessBoard[r + temp * j][c + temp * k];
+                            chessBoard[r][c] = " ";
+                            chessBoard[r + temp * j][c + temp * k] = "Q";
+                            if (kingSafe()) {
+                                list = list + r + c + (r + temp * j) + (c + temp * k) + oldPiece;
+                            }
+                            chessBoard[r][c] = "Q";
+                            chessBoard[r + temp * j][c + temp * k] = oldPiece;
+                            temp++;
                         }
-                        chessBoard[r][c] = "Q";
-                        chessBoard[r+temp*j][c+temp*k] = oldPiece;
-                        temp++;
-                    }
-                    if( (Character.isLowerCase(chessBoard[r+temp*j][c+temp*k].charAt(0)))){
-                        oldPiece = chessBoard[r+temp*j][c+temp*k];
-                        chessBoard[r][c] = " ";
-                        chessBoard[r+temp*j][c+temp*k] = "Q";
-                        if (kingSafe()) {
-                            list=list+r+c+(r+temp*j)+(c+temp*k)+oldPiece;
+                        if ((Character.isLowerCase(chessBoard[r + temp * j][c + temp * k].charAt(0)))) {
+                            oldPiece = chessBoard[r + temp * j][c + temp * k];
+                            chessBoard[r][c] = " ";
+                            chessBoard[r + temp * j][c + temp * k] = "Q";
+                            if (kingSafe()) {
+                                list = list + r + c + (r + temp * j) + (c + temp * k) + oldPiece;
+                            }
+                            chessBoard[r][c] = "Q";
+                            chessBoard[r + temp * j][c + temp * k] = oldPiece;
                         }
-                        chessBoard[r][c] = "Q";
-                        chessBoard[r+temp*j][c+temp*k] = oldPiece;
+                    } catch (Exception e) {
                     }
-                } catch (Exception e) {}
-                temp = 1;
+                    temp = 1;
+                }
             }
         }
         return list;
