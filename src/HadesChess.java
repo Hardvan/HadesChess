0f54938f8f5ea6c82cc622a7694f68efda0bd0ba
KaranS09 <karan.sathish980@gmail.com>
2023-08-03 02:28:09 +0530

    Added the functionality for knights

--- src/HadesChess.java
+++ src/HadesChess.java
@@ -8,7 +8,7 @@ public class HadesChess {
             {" "," "," "," "," "," "," "," "},
             {" "," "," "," "," "," "," "," "},
             {" "," "," "," "," "," "," "," "},
-            {"R"," "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "," "},
             {"P","P","P","P","P","P","P","P"},
             {"R","K","B","Q","A","B","K","R"}};
     static int kingPositionC, kingPositionL;
@@ -117,7 +117,34 @@ public class HadesChess {
         return list;
     }
     public static String posibleK(int i) {
-        String list="";
+        String list="", oldPiece;
+        int r=i/8, c=i%8;
+        for(int j=-1; j<=1; j+=2){
+            for(int k=-1;k<=1;k+=2){
+                try {
+                    if(Character.isLowerCase(chessBoard[r+j][c+k*2].charAt(0)) || " ".equals(chessBoard[r+j][c+k*2])){
+                        oldPiece = chessBoard[r+j][c+k*2];
+                        chessBoard[r][c] = " ";
+                        if (kingSafe()) {
+                            list = list + r + c + (r + j) + (c + k*2) + oldPiece;
+                        }
+                        chessBoard[r][c] = "K";
+                        chessBoard[r+j][c+k*2] = oldPiece;
+                    }
+                } catch (Exception e) {}
+                try {
+                    if(Character.isLowerCase(chessBoard[r+j*2][c+k].charAt(0)) || " ".equals(chessBoard[r+j*2][c+k])){
+                        oldPiece = chessBoard[r+j*2][c+k];
+                        chessBoard[r][c] = " ";
+                        if (kingSafe()) {
+                            list = list + r + c + (r + j*2) + (c + k) + oldPiece;
+                        }
+                        chessBoard[r][c] = "K";
+                        chessBoard[r+j*2][c+k] = oldPiece;
+                    }
+                } catch (Exception e) {}
+            }
+        }
         return list;
     }
     public static String posibleB(int i) {
