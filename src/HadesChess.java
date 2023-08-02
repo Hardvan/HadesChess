20a72f72fc077f9c887907a92a12d68881364e52
KaranS09 <karan.sathish980@gmail.com>
2023-08-03 01:45:54 +0530

    Added the implementation of the Rook

--- src/HadesChess.java
+++ src/HadesChess.java
@@ -8,7 +8,7 @@ public class HadesChess {
             {" "," "," "," "," "," "," "," "},
             {" "," "," "," "," "," "," "," "},
             {" "," "," "," "," "," "," "," "},
-            {" "," "," "," "," "," "," "," "},
+            {"R"," "," "," "," "," "," "," "},
             {"P","P","P","P","P","P","P","P"},
             {"R","K","B","Q","A","B","K","R"}};
     static int kingPositionC, kingPositionL;
@@ -61,7 +61,58 @@ public class HadesChess {
         return list;
     }
     public static String posibleR(int i) {
-        String list="";
+        String list="",oldPiece;
+        int r=i/8, c=i%8;
+        int temp = 1;
+        for(int j=-1; j<=1; j+=2){
+            try {
+                while(" ".equals(chessBoard[r][c+temp*j])){
+                    oldPiece = chessBoard[r][c+temp*j];
+                    chessBoard[r][c]= " ";
+                    chessBoard[r][c+temp*j] = "R";
+                    if (kingSafe()) {
+                        list = list + r + c + r + (c + temp * j) + oldPiece;
+                    }
+                    chessBoard[r][c] = "R";
+                    chessBoard[r][c+temp*j] = oldPiece;
+                    temp++;
+                }
+                if(Character.isLowerCase(chessBoard[r][c+temp*j].charAt(0))){
+                    oldPiece = chessBoard[r][c+temp*j];
+                    chessBoard[r][c]= " ";
+                    chessBoard[r][c+temp*j] = "R";
+                    if (kingSafe()) {
+                        list = list + r + c + r + (c + temp * j) + oldPiece;
+                    }
+                    chessBoard[r][c] = "R";
+                    chessBoard[r][c+temp*j] = oldPiece;
+                }
+            } catch (Exception e) {}
+            temp = 1;
+            try {
+                while(" ".equals(chessBoard[r+temp*j][c])){
+                    oldPiece = chessBoard[r+temp*j][c];
+                    chessBoard[r][c]= " ";
+                    chessBoard[r+temp*j][c] = "R";
+                    if (kingSafe()) {
+                        list = list + r + c + (r + temp*j) + c + oldPiece;
+                    }
+                    chessBoard[r][c] = "R";
+                    chessBoard[r+temp*j][c] = oldPiece;
+                    temp++;
+                }
+                if(Character.isLowerCase(chessBoard[r+temp*j][c].charAt(0))){
+                    oldPiece = chessBoard[r+temp*j][c];
+                    chessBoard[r][c]= " ";
+                    chessBoard[r+temp*j][c] = "R";
+                    if (kingSafe()) {
+                        list = list + r + c + (r+temp*j) + c + oldPiece;
+                    }
+                    chessBoard[r][c] = "R";
+                    chessBoard[r+temp*j][c] = oldPiece;
+                }
+            } catch (Exception e) {}
+        }
         return list;
     }
     public static String posibleK(int i) {
