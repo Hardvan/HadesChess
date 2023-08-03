dae26b2624361efe7401c63216a36d531d756928
KaranS09 <karan.sathish980@gmail.com>
2023-08-03 18:26:05 +0530

    added clauses for check and blocked locations

--- src/HadesChess.java
+++ src/HadesChess.java
@@ -13,6 +13,8 @@ public class HadesChess {
             {"R","K","B","Q","A","B","K","R"}};
     static int kingPositionC, kingPositionL;
     public static void main(String[] args) {
+        while(!"A".equals(chessBoard[kingPositionC/8][kingPositionC%8])) {kingPositionC++;}
+        while(!"a".equals(chessBoard[kingPositionL/8][kingPositionL%8])) {kingPositionL++;}
         /*
          * PIECE=WHITE/black
          * pawn=P/p
@@ -247,6 +249,88 @@ public class HadesChess {
         return list;
     }
     public static boolean kingSafe() {
+        //bishop/queen
+        int temp = 1;
+        for(int i=-1; i<=1; i+=2){
+            for(int j=-1; j<=1; j+=2){
+                    try {
+                        while (" ".equals(chessBoard[kingPositionC/8+temp*i][kingPositionC%8+temp*j])) {temp++;}
+                        if("b".equals(chessBoard[kingPositionC/8+temp*i][kingPositionC%8+temp*j]) || "q".equals(chessBoard[kingPositionC/8+temp*i][kingPositionC%8+temp*j])){
+                            return false;
+                        }
+                    } catch (Exception e) {
+                    }
+                    temp = 1;
+
+            }
+        }
+        //rook/queen
+        for(int i=-1; i<=1; i+=2){
+            try {
+                while (" ".equals(chessBoard[kingPositionC/8][kingPositionC%8+temp*i])) {temp++;}
+                if("b".equals(chessBoard[kingPositionC/8][kingPositionC%8+temp*i]) || "q".equals(chessBoard[kingPositionC/8][kingPositionC%8+temp*i])){
+                    return false;
+                }
+            } catch (Exception e) {
+            }
+            temp = 1;
+            try {
+                while (" ".equals(chessBoard[kingPositionC/8+temp*i][kingPositionC%8])) {temp++;}
+                if("b".equals(chessBoard[kingPositionC/8+temp*i][kingPositionC%8]) || "q".equals(chessBoard[kingPositionC/8+temp*i][kingPositionC%8])){
+                    return false;
+                }
+            } catch (Exception e) {
+            }
+            temp =1;
+        }
+        //knight
+        for(int i=-1; i<=1; i+=2){
+            for(int j=-1; j<=1; j+=2){
+                try {
+                    if("k".equals(chessBoard[kingPositionC/8+i][kingPositionC%8+j*2])){
+                        return false;
+                    }
+                } catch (Exception e) {
+                }
+                try {
+                    if("k".equals(chessBoard[kingPositionC/8+i*2][kingPositionC%8+j])){
+                        return false;
+                    }
+                } catch (Exception e) {
+                }
+
+            }
+        }
+        //pawn
+        if(kingPositionC >= 16) {
+            try {
+                if("p".equals(chessBoard[kingPositionC/8-1][kingPositionC%8-1])){
+                    return false;
+                }
+            } catch (Exception e) {
+            }
+            try {
+                if("p".equals(chessBoard[kingPositionC/8-1][kingPositionC%8+1])){
+                    return false;
+                }
+            } catch (Exception e) {
+            }
+        }
+        //king
+        for(int i=-1; i<=1; i++){
+            for(int j=-1; j<=1; j++){
+                if(i!=0 || j!=0){
+                    try {
+                        if("a".equals(chessBoard[kingPositionC/8+i][kingPositionC%8+j])){
+                            return false;
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+
+
+            }
+        }
         return true;
     }
 }
