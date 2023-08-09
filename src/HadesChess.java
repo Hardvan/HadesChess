3f3cd77bc04b88faf8701b86973bcd061e245370
KaranS09 <karan.sathish980@gmail.com>
2023-08-09 18:24:01 +0530

    added the makeMove and undoMove functions

--- src/HadesChess.java
+++ src/HadesChess.java
@@ -1,5 +1,7 @@
 import com.sun.source.tree.TryTree;
 
+import java.util.*;
+
 //import javax.swing.*;
 public class HadesChess {
     static String chessBoard[][]={
@@ -36,7 +38,36 @@ public class HadesChess {
         f.add(ui);
         f.setSize(500, 500);
         f.setVisible(true);*/
+        makeMove("6555 ");
+        undoMove("6555 ");
         System.out.println(posibleMoves());
+        for(int i=0;i<8;i++){
+            System.out.println(Arrays.toString(chessBoard[i]));
+        }
+    }
+
+    public static void makeMove(String move){
+        if (move.charAt(4) != 'P'){
+            //x1,y1,x2,y2,captured piece
+            chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))] = chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))];
+            chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))] = " ";
+        } else { //if pawn promotion
+            //column1,column2,captured-piece,new-piece,P
+            chessBoard[1][Character.getNumericValue(move.charAt(0))] = " ";
+            chessBoard[0][Character.getNumericValue(move.charAt(1))] = String.valueOf(move.charAt(3));
+        }
+    }
+
+    public static void undoMove(String move){
+        if (move.charAt(4) != 'P'){
+            //x1,y1,x2,y2,captured piece
+            chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))] = chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))];
+            chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))] = String.valueOf(move.charAt(4));
+        } else { //if pawn promotion
+            //column1,column2,captured-piece,new-piece,P
+            chessBoard[1][Character.getNumericValue(move.charAt(0))] = "P";
+            chessBoard[0][Character.getNumericValue(move.charAt(1))] = String.valueOf(move.charAt(2));
+        }
     }
     public static String posibleMoves() {
         String list="";
