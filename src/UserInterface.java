38426f570f3b1c1e4310f0a56681d79811ab817f
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-08-15 17:10:48 +0530

    Code readability changes

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,53 +1,79 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class UserInterface extends JPanel implements MouseListener, MouseMotionListener{
+
+public class UserInterface extends JPanel implements MouseListener, MouseMotionListener {
     static int mouseX, mouseY, newMouseX, newMouseY;
-    static int squareSize=64;
+    static int squareSize = 64;
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.setBackground(Color.yellow);
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
-        for (int i=0;i<64;i+=2) {
-            g.setColor(new Color(255,200,100));
-            g.fillRect((i%8+(i/8)%2)*squareSize, (i/8)*squareSize, squareSize, squareSize);
-            g.setColor(new Color(150,50,30));
-            g.fillRect(((i+1)%8-((i+1)/8)%2)*squareSize, ((i+1)/8)*squareSize, squareSize, squareSize);
+        for (int i = 0; i < 64; i += 2) {
+            g.setColor(new Color(255, 200, 100));
+            g.fillRect((i % 8 + (i / 8) % 2) * squareSize, (i / 8) * squareSize, squareSize, squareSize);
+            g.setColor(new Color(150, 50, 30));
+            g.fillRect(((i + 1) % 8 - ((i + 1) / 8) % 2) * squareSize, ((i + 1) / 8) * squareSize, squareSize, squareSize);
         }
         Image chessPiecesImage;
-        chessPiecesImage=new ImageIcon("ChessPieces.png").getImage();
-        for (int i=0;i<64;i++) {
-            int j=-1,k=-1;
-            switch (HadesChess.chessBoard[i/8][i%8]) {
-                case "P": j=5; k=0;
+        chessPiecesImage = new ImageIcon("ChessPieces.png").getImage();
+        for (int i = 0; i < 64; i++) {
+            int j = -1, k = -1;
+            switch (HadesChess.chessBoard[i / 8][i % 8]) {
+                case "P":
+                    j = 5;
+                    k = 0;
                     break;
-                case "p": j=5; k=1;
+                case "p":
+                    j = 5;
+                    k = 1;
                     break;
-                case "R": j=2; k=0;
+                case "R":
+                    j = 2;
+                    k = 0;
                     break;
-                case "r": j=2; k=1;
+                case "r":
+                    j = 2;
+                    k = 1;
                     break;
-                case "K": j=4; k=0;
+                case "K":
+                    j = 4;
+                    k = 0;
                     break;
-                case "k": j=4; k=1;
+                case "k":
+                    j = 4;
+                    k = 1;
                     break;
-                case "B": j=3; k=0;
+                case "B":
+                    j = 3;
+                    k = 0;
                     break;
-                case "b": j=3; k=1;
+                case "b":
+                    j = 3;
+                    k = 1;
                     break;
-                case "Q": j=1; k=0;
+                case "Q":
+                    j = 1;
+                    k = 0;
                     break;
-                case "q": j=1; k=1;
+                case "q":
+                    j = 1;
+                    k = 1;
                     break;
-                case "A": j=0; k=0;
+                case "A":
+                    j = 0;
+                    k = 0;
                     break;
-                case "a": j=0; k=1;
+                case "a":
+                    j = 0;
+                    k = 1;
                     break;
             }
-            if (j!=-1 && k!=-1) {
-                g.drawImage(chessPiecesImage, (i%8)*squareSize, (i/8)*squareSize, (i%8+1)*squareSize, (i/8+1)*squareSize, j*64, k*64, (j+1)*64, (k+1)*64, this);
+            if (j != -1 && k != -1) {
+                g.drawImage(chessPiecesImage, (i % 8) * squareSize, (i / 8) * squareSize, (i % 8 + 1) * squareSize, (i / 8 + 1) * squareSize, j * 64, k * 64, (j + 1) * 64, (k + 1) * 64, this);
             }
         }
         /*g.setColor(Color.BLUE);
@@ -57,34 +83,38 @@ public class UserInterface extends JPanel implements MouseListener, MouseMotionL
         g.drawString("Jonathan", x, y);
         */
     }
+
     @Override
-    public void mouseMoved(MouseEvent e) {}
+    public void mouseMoved(MouseEvent e) {
+    }
+
     @Override
     public void mousePressed(MouseEvent e) {
-        if (e.getX()<8*squareSize &&e.getY()<8*squareSize) {
+        if (e.getX() < 8 * squareSize && e.getY() < 8 * squareSize) {
             //if inside the board
-            mouseX=e.getX();
-            mouseY=e.getY();
+            mouseX = e.getX();
+            mouseY = e.getY();
             repaint();
         }
     }
+
     @Override
     public void mouseReleased(MouseEvent e) {
-        if (e.getX()<8*squareSize &&e.getY()<8*squareSize) {
+        if (e.getX() < 8 * squareSize && e.getY() < 8 * squareSize) {
             //if inside the board
-            newMouseX=e.getX();
-            newMouseY=e.getY();
-            if (e.getButton()==MouseEvent.BUTTON1) {
+            newMouseX = e.getX();
+            newMouseY = e.getY();
+            if (e.getButton() == MouseEvent.BUTTON1) {
                 String dragMove;
-                if (newMouseY/squareSize==0 && mouseY/squareSize==1 && "P".equals(HadesChess.chessBoard[mouseY/squareSize][mouseX/squareSize])) {
+                if (newMouseY / squareSize == 0 && mouseY / squareSize == 1 && "P".equals(HadesChess.chessBoard[mouseY / squareSize][mouseX / squareSize])) {
                     //pawn promotion
-                    dragMove=""+mouseX/squareSize+newMouseX/squareSize+HadesChess.chessBoard[newMouseY/squareSize][newMouseX/squareSize]+"QP";
+                    dragMove = "" + mouseX / squareSize + newMouseX / squareSize + HadesChess.chessBoard[newMouseY / squareSize][newMouseX / squareSize] + "QP";
                 } else {
                     //regular move
-                    dragMove=""+mouseY/squareSize+mouseX/squareSize+newMouseY/squareSize+newMouseX/squareSize+HadesChess.chessBoard[newMouseY/squareSize][newMouseX/squareSize];
+                    dragMove = "" + mouseY / squareSize + mouseX / squareSize + newMouseY / squareSize + newMouseX / squareSize + HadesChess.chessBoard[newMouseY / squareSize][newMouseX / squareSize];
                 }
-                String userPosibilities=HadesChess.posibleMoves();
-                if (userPosibilities.replaceAll(dragMove, "").length()<userPosibilities.length()) {
+                String userPosibilities = HadesChess.posibleMoves();
+                if (userPosibilities.replaceAll(dragMove, "").length() < userPosibilities.length()) {
                     //if valid move
                     HadesChess.makeMove(dragMove);
                     HadesChess.flipBoard();
@@ -95,12 +125,20 @@ public class UserInterface extends JPanel implements MouseListener, MouseMotionL
             }
         }
     }
+
     @Override
-    public void mouseClicked(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e) {
+    }
+
     @Override
-    public void mouseDragged(MouseEvent e) {}
+    public void mouseDragged(MouseEvent e) {
+    }
+
     @Override
-    public void mouseEntered(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {
+    }
+
     @Override
-    public void mouseExited(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {
+    }
 }
